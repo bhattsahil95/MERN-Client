834679f7ecbe2bc7e922dd4a18330b8db347092f
bhattsahil95 <bhattsahil95@gmail.com>
2023-08-16 00:14:28 -0600

    Improved Home page

---
+++ src/assets/badgeData.js
@@ -0,0 +1,128 @@
+// badgeData.js
+const badgeData = [
+	{
+		link: "https://getbootstrap.com",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+		alt: "Bootstrap",
+		type: "Front-end",
+	},
+	{
+		link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+		alt: "CSS",
+		type: "Front-end",
+	},
+
+	{
+		link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+		alt: "HTML",
+		type: "Front-end",
+	},
+
+	{
+		link: "https://nodejs.org",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-plain-wordmark.svg",
+		alt: "Node.js",
+		type: "Back-end ",
+	},
+	{
+		link: "https://www.mysql.com",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+		alt: "MySQL",
+		type: "Database",
+	},
+	{
+		link: "https://flask.palletsprojects.com",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original-wordmark.svg",
+		alt: "Flask",
+		type: "Back-end ",
+	},
+	{
+		link: "https://git-scm.com",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original-wordmark.svg",
+		alt: "Git",
+		type: "Tools",
+	},
+	{
+		link: "https://github.com",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original-wordmark.svg",
+		alt: "GitHub",
+		type: "Tools",
+	},
+	{
+		link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+		alt: "JavaScript",
+		type: "Front-end",
+	},
+	{
+		link: "https://www.linux.org",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+		alt: "Linux",
+		type: "Operating System",
+	},
+	{
+		link: "https://www.microsoft.com/en-us/windows",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/windows8/windows8-original.svg",
+		alt: "Windows",
+		type: "Operating System",
+	},
+	{
+		link: "https://www.mongodb.com",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+		alt: "MongoDB",
+		type: "Database",
+	},
+	{
+		link: "https://reactjs.org",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+		alt: "React",
+		type: "Front-end",
+	},
+	{
+		link: "https://www.python.org",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
+		alt: "Python",
+		type: "Back-end ",
+	},
+	{
+		link: "https://code.visualstudio.com/",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/visualstudio/visualstudio-plain.svg",
+		alt: "Visual Studio Code",
+		type: "Tools",
+	},
+	{
+		link: "https://jupyter.org/",
+		imageSrc:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/jupyter/jupyter-original.svg",
+		alt: "Jupyter Notebook",
+		type: "Tools",
+	},
+	{
+		link: "https://www.jetbrains.com/pycharm/",
+		imageSrc:
+			"https://upload.wikimedia.org/wikipedia/commons/1/1d/PyCharm_Icon.svg",
+		alt: "PyCharm",
+		type: "Tools",
+	},
+
+	// Add more badge data objects as needed
+];
+
+export default badgeData;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,55 +1,126 @@
 import React, { useEffect, useState } from "react";
 import particleConfig from "../assets/particles.json";
 import Typewriter from "typewriter-effect";
+import badgeData from "../assets/badgeData";
+
+import {
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+	Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const HomePage = () => {
-  const titles = ["   Finance Professional", "   Tech Enthusiast", "   Gamer"];
+	const titles = [
+		"   Finance Professional",
+		"   Tech Enthusiast",
+		"   Gamer",
+	];
 
-  useEffect(() => {
-    // Initialize particles effect only if on the home page
-    if (window.location.pathname === "/") {
-      /* eslint-disable no-undef */
-      particlesJS("particles-js", particleConfig); // Use your particleConfig here
-      /* eslint-enable no-undef */
-    }
-  }, []); // Empty dependency array ensures this effect runs only once
+	// Initialize particles effect only if on the home page
+	useEffect(() => {
+		if (window.location.pathname === "/") {
+			/* eslint-disable no-undef */
+			particlesJS("particles-js", particleConfig); // Use your particleConfig here
+			/* eslint-enable no-undef */
+		}
+	}, []);
 
-  return (
-    <div className="home-page">
-      <div id="particles-js">
-        <div className="intro">
-          <div className="home-page-header">
-            <h1>Hi, I'm Sahil Bhatt 👋 </h1>
-            <Typewriter
-              options={{
-                strings: titles,
-                autoStart: true,
-                cursor: "|",
-                loop: true,
-                wrapperClassName: "typewriter-wrapper",
-              }}
-            />
-          </div>
-          <p className="intro-text">
-            Welcome to my world! I'm Sahil Bhatt, an enthusiast with a penchant
-            for all things tech. While my journey started in the realm of
-            commerce, my fascination with technology has been an unwavering
-            force, propelling me to explore its boundless possibilities. As a
-            numbers enthusiast by profession, I've mastered the art of managing
-            financial landscapes. But beyond the spreadsheets and balance
-            sheets, I'm driven by a curiosity to unravel the intricate tapestry
-            of the digital world. Join me as I bridge the gap between finance
-            and technology, unraveling insights and discoveries that merge two
-            seemingly distinct domains into a harmonious symphony of innovation.
-          </p>
-        </div>
+	const groupBadgesByType = () => {
+		const groupedBadges = {};
+		badgeData.forEach((badge) => {
+			if (!groupedBadges[badge.type]) {
+				groupedBadges[badge.type] = [];
+			}
+			groupedBadges[badge.type].push(badge);
+		});
+		return groupedBadges;
+	};
 
-        <div className="home-info-section">
-          
-        </div>
-      </div>
-    </div>
-  );
+	const groupedBadges = groupBadgesByType();
+
+	return (
+		<div className="home-page">
+			<div className="intro">
+				<div className="partcles" id="particles-js"></div>
+				<div className="home-page-header">
+					<h1>Hi, I'm Sahil Bhatt 👋 </h1>
+					<Typewriter
+						options={{
+							strings: titles,
+							autoStart: true,
+							cursor: "|",
+							loop: true,
+							wrapperClassName: "typewriter-wrapper",
+						}}
+					/>
+				</div>
+				<p className="intro-text">
+					<span className="into-text-content">
+						Welcome to my world! I'm Sahil Bhatt, an enthusiast with
+						a penchant for all things tech. While my journey started
+						in the realm of commerce, my fascination with technology
+						has been an unwavering force, propelling me to explore
+						its boundless possibilities. As a numbers enthusiast by
+						profession, I've mastered the art of managing financial
+						landscapes. But beyond the spreadsheets and balance
+						sheets, I'm driven by a curiosity to unravel the
+						intricate tapestry of the digital world. Join me as I
+						bridge the gap between finance and technology,
+						unraveling insights and discoveries that merge two
+						seemingly distinct domains into a harmonious symphony of
+						innovation.
+					</span>
+				</p>
+			</div>
+
+			<div className="tech-info-section">
+				<h1>My Tech Stack </h1>
+				<div className="block">
+					<div disableGutters className="stack-container">
+						{Object.keys(groupedBadges).map((type) => (
+							<Accordion
+								disableGutters
+								defaultExpanded
+								className={`stack-box`}
+								id={type}
+								key={type}
+							>
+								<AccordionSummary
+									expandIcon={<ExpandMoreIcon />}
+								>
+									<Typography
+										className="stack-title"
+										variant="h6"
+									>
+										{type}
+									</Typography>
+								</AccordionSummary>
+								<AccordionDetails className="stack-badges">
+									{groupedBadges[type].map((badge, index) => (
+										<div key={index}>
+											<a
+												href={badge.link}
+												target="_blank"
+												rel="noreferrer"
+											>
+												<img
+													className="stack-item"
+													src={badge.imageSrc}
+													alt={badge.alt}
+												/>
+											</a>
+										</div>
+									))}
+								</AccordionDetails>
+							</Accordion>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
